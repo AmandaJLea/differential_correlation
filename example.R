6dbd025ab3b3f1e71829d4e18c54b7415de9d520
Amanda Lea <amandalea7180@gmail.com>
2018-10-17 08:45:18 -0700

    Update example.R

--- example.R
+++ example.R
@@ -76,6 +76,7 @@ results <- as.data.frame(t(Reduce(cbind,lapply(1:dim(pairs)[1], function(i) {
 tmp1<-(e_all_norm[pairs$V1[i],])
 tmp2<-(e_all_norm[pairs$V2[i],])
 # here, a wilcoxon test is used to test for differences in correlation between groups; however, any number of other statistical methods (e.g., linear models or linear mixed effects models) could be used in the same framework
+# to use an alternate statistical approach, extract the product matrix and use it as the outcome variable in another type of statistical model  
 return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,method='spearman')$estimate, wilcox.test( (tmp1*tmp2) ~ predictor)$p.value))
 }))))
 names(results)<-c('cor_healthy','cor_sick','p_value')
