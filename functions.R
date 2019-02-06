48c45d6409fc265e9e960ca4c211bd0fd18d7404
Amanda Lea <amandalea7180@gmail.com>
2019-02-06 15:58:43 -0500

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,9 +1,9 @@
-diff_correlation_2grps=function(input_matrix,input_predictor,class1_name,class2_name,plot=T) {
+CILP_withLM=function(input_matrix,predictor_DF,predictor_column,covariates_column_start,covariates_column_end,class1_name,class2_name,plot=T) {
 
 # get all possible pairwise combinations of genes
-genes=dim(data)[1]
+genes=dim(input_matrix)[1]
 e_all<-input_matrix
-predictor<-input_predictor
+predictor<-predictor_DF[,predictor_column]
 pairs<-as.data.frame(t(combn( 1:genes, 2)))
 
 print('scaling data within the classes to be compared')
@@ -15,11 +15,13 @@ tmp[i,which(predictor==0)]<-scale( t(e_all[i,which(predictor==0)] ))
 e_all_norm<-tmp
 
 print('testing for differences in correlation')
+
+model<-model.matrix(~predictor_DF[,predictor_column] + as.matrix(predictor_DF[,covariates_column_start:covariates_column_end]) )
+
 results <- as.data.frame(t(Reduce(cbind,lapply(1:dim(pairs)[1], function(i) {
 tmp1<-(e_all_norm[pairs$V1[i],])
 tmp2<-(e_all_norm[pairs$V2[i],])
-# here, a wilcoxon test is used to test for differences in correlation between groups; however, any number of other statistical methods (e.g., linear models or linear mixed effects models) could be used in the same framework
-return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,method='spearman')$estimate, wilcox.test( (tmp1*tmp2) ~ predictor)$p.value))
+return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,method='spearman')$estimate, summary(lm( scale(tmp1*tmp2) ~ model))$coefficients[2,4]))
 }))))
 names(results)<-c(paste('cor',class1_name,sep='_'),paste('cor',class2_name,sep='_'),'p_value')
 print('saving results')
@@ -29,24 +31,10 @@ if(plot==T) {
 print('plotting results')
 par(mfrow=c(2,2))
 
-# volcano plot
-library(qvalue)
-results$qvalue<-qvalue(results$p_value)$qvalues
-plot(results$cor_healthy - results$cor_sick,-log10(results$p_value),bty='n',col=as.factor(results$qvalue<0.05),xlab='difference in correlation',ylab='-log10, p-value')
-
 # qq-plot
 qqplot(-log10(runif(dim(pairs)[1])),-log10(results$p_value),pch=20,xlab='p-value, uniform distribution',ylab='p-value, differential correlation test',bty='n',xlim=c(0,max(-log10(results$p_value))),ylim=c(0,max(-log10(results$p_value))))
 x=c(0,1);y=c(0,1);abline(lm(y~x),lty=2)
 
-# example
-i<-which( results$p_value==min(results$p_value))
-plot( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,bty='n',xlab='Normalized expression of gene 1',ylab='Normalized expression of gene 2',main=class1_name)
-abline(lm ( e_all_norm[pairs$V2[i],which(predictor==0)] ~ e_all_norm[pairs$V1[i],which(predictor==0)] ),lty=2)
-
-plot( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,bty='n',xlab='Normalized expression of gene 1',ylab='Normalized expression of gene 2',main=class2_name)
-abline(lm ( e_all_norm[pairs$V2[i],which(predictor==1)] ~ e_all_norm[pairs$V1[i],which(predictor==1)] ),lty=2)
-
 } 
 print('done')
 }
-  
