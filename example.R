acffec11d7998a802bda551ea60efa3c0195b85d
Amanda Lea <amandalea7180@gmail.com>
2019-02-06 15:00:33 -0500

    Update example.R

--- example.R
+++ example.R
@@ -62,8 +62,8 @@ pairs<-as.data.frame(t(combn( 1:genes, 2)))
 # scale data within the classes to be compared
 tmp<-matrix(nrow=dim(e_all)[1],ncol=dim(e_all)[2])
 for (i in c(1:dim(tmp)[1])) {
-tmp[i,which(predictor==1)]<-scale( t(e_all[i,which(predictor==1)] ))
-tmp[i,which(predictor==0)]<-scale( t(e_all[i,which(predictor==0)] ))
+tmp[i,which(predictor==1)]<-scale( t(e_all[i,which(predictor==1)] ),center = TRUE, scale = TRUE)
+tmp[i,which(predictor==0)]<-scale( t(e_all[i,which(predictor==0)] ),center = TRUE, scale = TRUE)
 } 
 e_all_norm<-tmp
 
@@ -71,13 +71,17 @@ e_all_norm<-tmp
 p2<-apply(e_all_norm,1,function(x) summary(lm(x ~ predictor))$coefficients[2,4])
 summary(p2)
 
+# confirm there are no/minimal variance differences in expression either
+p2<-apply(e_all_norm,1,function(x) var(x[which(predictor==0)]) - var(x[which(predictor==1)]))
+summary(p2)
+
 # test for differences in correlation (this will take a few minutes to run, depending on sample size and gene number)
 results <- as.data.frame(t(Reduce(cbind,lapply(1:dim(pairs)[1], function(i) {
 tmp1<-(e_all_norm[pairs$V1[i],])
 tmp2<-(e_all_norm[pairs$V2[i],])
-# here, a wilcoxon test is used to test for differences in correlation between groups; however, any number of other statistical methods (e.g., linear models or linear mixed effects models) could be used in the same framework
+# here, a linear model is used to test for differences in correlation between groups; however, any number of other statistical methods (e.g., linear models with additional covariates or linear mixed effects models) could be used in the same framework
 # to use an alternate statistical approach, extract the product matrix and use it as the outcome variable in another type of statistical model  
-return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,method='spearman')$estimate, wilcox.test( (tmp1*tmp2) ~ predictor)$p.value))
+return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor==0)] , e_all_norm[pairs$V2[i],which(predictor==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,method='spearman')$estimate, summary(lm( scale(tmp1*tmp2) ~ predictor))$coefficients[2,4]))
 }))))
 names(results)<-c('cor_healthy','cor_sick','p_value')
 
@@ -90,7 +94,7 @@ par(mfrow=c(2,2))
 # volcano plot
 library(qvalue)
 results$qvalue<-qvalue(results$p_value)$qvalues
-plot(results$cor_healthy - results$cor_sick,-log10(results$p_value),bty='n',col=as.factor(results$qvalue<0.05),xlab='difference in correlation (healthy-sick)',ylab='-log10, p-value')
+plot(results$cor_healthy - results$cor_sick,-log10(results$p_value),bty='n',col=as.factor(results$qvalue<0.05),xlab='difference in Spearman correlation (healthy-sick)',ylab='-log10, p-value')
 
 # qq-plot
 qqplot(-log10(runif(dim(pairs)[1])),-log10(results$p_value),pch=20,xlab='p-value, uniform distribution',ylab='p-value, differential correlation test',bty='n',xlim=c(0,max(-log10(results$p_value))),ylim=c(0,max(-log10(results$p_value))))
@@ -104,3 +108,56 @@ abline(lm ( e_all_norm[pairs$V2[i],which(predictor==0)] ~ e_all_norm[pairs$V1[i]
 plot( e_all_norm[pairs$V1[i],which(predictor==1)] , e_all_norm[pairs$V2[i],which(predictor==1)] ,bty='n',xlab='Normalized expression of gene 1',ylab='Normalized expression of gene 2',main='Sick group')
 abline(lm ( e_all_norm[pairs$V2[i],which(predictor==1)] ~ e_all_norm[pairs$V1[i],which(predictor==1)] ),lty=2)
 
+############
+# check null expectations through permutation
+# permute the sample labels before manipulating the gene expression data
+############
+
+predictor_perm<-sample(predictor)
+
+# scale data within the classes to be (permuted) compared
+tmp<-matrix(nrow=dim(e_all)[1],ncol=dim(e_all)[2])
+for (i in c(1:dim(tmp)[1])) {
+tmp[i,which(predictor_perm==1)]<-scale( t(e_all[i,which(predictor_perm==1)] ),center = TRUE, scale = TRUE)
+tmp[i,which(predictor_perm==0)]<-scale( t(e_all[i,which(predictor_perm==0)] ),center = TRUE, scale = TRUE)
+} 
+e_all_norm<-tmp
+
+# test for differences in correlation using permuted predictor variable (this will take a few minutes to run, depending on sample size and gene number)
+results_perm <- as.data.frame(t(Reduce(cbind,lapply(1:dim(pairs)[1], function(i) {
+tmp1<-(e_all_norm[pairs$V1[i],])
+tmp2<-(e_all_norm[pairs$V2[i],])
+return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor_perm==0)] , e_all_norm[pairs$V2[i],which(predictor_perm==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor_perm==1)] , e_all_norm[pairs$V2[i],which(predictor_perm==1)] ,method='spearman')$estimate, summary(lm( scale(tmp1*tmp2) ~ predictor_perm))$coefficients[2,4]))
+}))))
+names(results_perm)<-c('cor_healthy','cor_sick','p_value')
+
+# qq-plot - should not show any enrichment of low p-values relative to a uniform distribution  
+qqplot(-log10(runif(dim(pairs)[1])),-log10(results_perm$p_value),pch=20,xlab='p-value, uniform distribution',ylab='p-value, differential correlation test',bty='n',xlim=c(0,max(-log10(results_perm$p_value))),ylim=c(0,max(-log10(results_perm$p_value))))
+x=c(0,1);y=c(0,1);abline(lm(y~x),lty=2)
+
+############
+# check null expectations through permutation
+# permute the sample labels before running statistical tests (shown here = linear models)
+############
+
+predictor_perm<-sample(predictor)
+
+# scale data within the (non permuted) classes to be compared
+tmp<-matrix(nrow=dim(e_all)[1],ncol=dim(e_all)[2])
+for (i in c(1:dim(tmp)[1])) {
+tmp[i,which(predictor==1)]<-scale( t(e_all[i,which(predictor==1)] ),center = TRUE, scale = TRUE)
+tmp[i,which(predictor==0)]<-scale( t(e_all[i,which(predictor==0)] ),center = TRUE, scale = TRUE)
+} 
+e_all_norm<-tmp
+
+# test for differences in correlation using permuted predictor variable (this will take a few minutes to run, depending on sample size and gene number)
+results_perm2 <- as.data.frame(t(Reduce(cbind,lapply(1:dim(pairs)[1], function(i) {
+tmp1<-(e_all_norm[pairs$V1[i],])
+tmp2<-(e_all_norm[pairs$V2[i],])
+return( c( cor.test( e_all_norm[pairs$V1[i],which(predictor_perm==0)] , e_all_norm[pairs$V2[i],which(predictor_perm==0)] ,method='spearman')$estimate, cor.test( e_all_norm[pairs$V1[i],which(predictor_perm==1)] , e_all_norm[pairs$V2[i],which(predictor_perm==1)] ,method='spearman')$estimate, summary(lm( scale(tmp1*tmp2) ~ predictor_perm))$coefficients[2,4]))
+}))))
+names(results_perm2)<-c('cor_healthy','cor_sick','p_value')
+
+# qq-plot - should not show any enrichment of low p-values relative to a uniform distribution  
+qqplot(-log10(runif(dim(pairs)[1])),-log10(results_perm2$p_value),pch=20,xlab='p-value, uniform distribution',ylab='p-value, differential correlation test',bty='n',xlim=c(0,max(-log10(results_perm2$p_value))),ylim=c(0,max(-log10(results_perm2$p_value))))
+x=c(0,1);y=c(0,1);abline(lm(y~x),lty=2)
